7e3d994213468f2dbad932136fddc72cd1d6fd61
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-28 16:48:19 +0100

    Reloj

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
+function añadir0(numero) {
+  if (numero < 10) {
+    var tmp = new String (numero);
+    var tmp2 = '0' + tmp;
+    numero = new Number(tmp2);
+  }
+  else {
+    var tmp2 = new String (numero);
+  }
+  return tmp2;
+}
+
 var actualiza = function (){
   var currentDate = new Date();
-  var horaActual = currentDate.getHours();
-  var minutoActual = currentDate.getMinutes();
-  var segundoActual = currentDate.getSeconds();
 
   var hora = document.getElementById('horas');
   var minutos = document.getElementById('minutos');
   var segundos = document.getElementById('segundos');
 
-  hora.innerHTML = horaActual;
-  minutos.innerHTML = minutoActual;
-  segundos.innerHTML = segundoActual;
+  hora.innerHTML = añadir0(currentDate.getHours());
+  minutos.innerHTML = añadir0(currentDate.getMinutes());
+  segundos.innerHTML = añadir0(currentDate.getSeconds());
 };
 
 var intervalo = setInterval(actualiza, 1000);
